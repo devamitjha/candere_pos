4d271efd8de919185d0dbe5a261e041c94102ccf
amitjha25in <amit.jha@candere.com>
2024-12-05 15:39:45 +0530

    add customer

--- src/components/searchbox/Searchbox.js
+++ src/components/searchbox/Searchbox.js
@@ -335,7 +335,7 @@ const Searchbox = () => {
             let config = {
                 method: 'post',
                 maxBodyLength: Infinity,
-                url: '/rest/V1/pos-customermanagement/registerSignup', 
+                url: '/api/V1/pos-customermanagement/registerSignup', 
                 headers: { 
                     'Content-Type': 'application/json', 
                     'Authorization': 'Bearer 52al19ff4wb6z8hysbr9y36cmit3ueop', 
