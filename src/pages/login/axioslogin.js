107f4d05b83ba3184171ff8aef9b28f8e3267c79
amitjha25in <amit.jha@candere.com>
2024-12-10 17:53:53 +0530

    token update

--- src/pages/login/axioslogin.js
+++ src/pages/login/axioslogin.js
@@ -70,7 +70,7 @@ const Login = () => {
       url: '/api/V1/pos-agentmanagement/agentlogin',
       headers: { 
         'Content-Type': 'application/json', 
-        'Authorization': 'Bearer 52al19ff4wb6z8hysbr9y36cmit3ueop',
+        'Authorization': 'Bearer rfu5fspn4e8o97zqls7batatdjvq2c77',
       },
       withCredentials: true,
       data: data
--- src/services/fetchProductData.js
+++ src/services/fetchProductData.js
@@ -8,7 +8,7 @@ export const fetchProductsData = async (storeCode, agentCodeOrPhone, customer_id
     url: '/api/V1/pos-productmanagement/getproducts',
     headers: {
         'Content-Type': 'application/json',
-        Authorization: 'Bearer 52al19ff4wb6z8hysbr9y36cmit3ueop',
+        Authorization: 'Bearer rfu5fspn4e8o97zqls7batatdjvq2c77',
     },
     data: JSON.stringify({
         storeCode:storeCode ,
